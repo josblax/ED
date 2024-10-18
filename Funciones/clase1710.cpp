17706e7e28566c7a01c79ba920306ca651cf16d8
Jose Blancas <92336331+josblax@users.noreply.github.com>
2024-10-17 20:33:49 -0600

    Create clase1710.cpp

---
+++ Funciones/clase1710.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplication19.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Header.h"
+using namespace std;
+
+int main()
+{
+	ej1();
+}
