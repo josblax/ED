51b2727368cfc880b3588191d472f945b1f790b0
Jose Blancas <92336331+josblax@users.noreply.github.com>
2024-10-21 20:28:38 -0600

    Create claseAlumno.cpp

---
+++ Clases/claseAlumno.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+// class
+class Alumno 
+{
+	//atributos
+	int numCuenta;
+	string nombreCompleto;
+	int materiasCursadas;
+	int materiasCursando;
+	int materiasPorCursar;
+	//constructor
+	Alumno(int nc, string nombre)
+	{
+		this->numCuenta = nc;
+		this->nombreCompleto = nombre;
+	}
+
+};
+
+int main()
+{
+
+}
