614b81805e635e19a50e80e14b0996d3f6903380
Jose Blancas <92336331+josblax@users.noreply.github.com>
2024-11-14 16:20:49 -0600

    Create Musica.cpp

---
+++ Clases/Libreria/Musica.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Material.h"
+using namespace std;
+
+class Musica :public Material
+{
+private:
+	string artista;
+	string album;
+	vector<string> canciones;
+	
+public:
+	Musica(string id, string titulo, string genero, string tipo, string region, int cantidad, bool referenciaLimitada, int annio,
+		string artista,
+		string album,
+		vector<string> canciones) : Material(id, titulo, genero, tipo, region, cantidad, referenciaLimitada, annio),
+		album(album), canciones(canciones), artista(artista) {}
+	// Getters 
+		string getArtista() const { return artista; } 
+		string getAlbum() const { return album; } 
+		vector<string> getCanciones() const { return canciones; } 
+		// Setters 
+		void setArtista(const string& artista) { this->artista = artista; } 
+		void setAlbum(const string& album) { this->album = album; } 
+		void setCanciones(const vector<string>& canciones) { this->canciones = canciones; }
+};
+	
