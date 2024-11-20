1bc3ff250aea53c9d1c148f002a396c3599d290c
Jose Blancas <92336331+josblax@users.noreply.github.com>
2024-11-20 10:20:15 -0600

    Update Material.h

--- Clases/Libreria/Material.h
+++ Clases/Libreria/Material.h
@@ -10,7 +10,6 @@ protected:
     string titulo;
     string genero;
     string tipo;
-    double duracion;
     string region;
     int cantidad;
     bool referenciaLimitada;
@@ -19,29 +18,27 @@ protected:
 public:
     // Constructor
     Material(string id, string titulo, string genero, string tipo, string region, int cantidad, bool referenciaLimitada, int annio)
-        : id(id), titulo(titulo), genero(genero), tipo(tipo), duracion(0), region(region), cantidad(cantidad), referenciaLimitada(referenciaLimitada), annio(annio) {}
+        : id(id), titulo(titulo), genero(genero), tipo(tipo), region(region), cantidad(cantidad), referenciaLimitada(referenciaLimitada), annio(annio) {}
 
     // Getters
     string getId() const { return id; }
     string getTitulo() const { return titulo; }
     string getGenero() const { return genero; }
     string getTipo() const { return tipo; }
-    double getDuracion() const { return duracion; }
     string getRegion() const { return region; }
     int getCantidad() const { return cantidad; }
     bool getReferenciaLimitada() const { return referenciaLimitada; }
-    int getannio() const { return annio;  }
+    int getannio() const { return annio; }
 
     // Setters
     void setId(const string& id) { this->id = id; }
     void setTitulo(const string& titulo) { this->titulo = titulo; }
     void setGenero(const string& genero) { this->genero = genero; }
     void setTipo(const string& tipo) { this->tipo = tipo; }
-    void setDuracion(double duracion) { this->duracion = duracion; }
     void setRegion(const string& region) { this->region = region; }
     void setCantidad(int cantidad) { this->cantidad = cantidad; }
     void setReferenciaLimitada(bool referenciaLimitada) { this->referenciaLimitada = referenciaLimitada; }
-    void setAnnio(int annio) { this->annio = annio;  }
+    void setAnnio(int annio) { this->annio = annio; }
 };
 
 #endif
