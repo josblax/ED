beffab5db1ad6e2722e839a0c325d0fd17c241e6
Jose Blancas <92336331+josblax@users.noreply.github.com>
2024-12-02 21:23:48 -0600

    Update Musica.cpp

--- Clases/Libreria/Musica.cpp
+++ Clases/Libreria/Musica.cpp
@@ -10,7 +10,7 @@ private:
 	string artista;
 	string album;
 	vector<string> canciones;
-	
+
 public:
 	Musica(string id, string titulo, string genero, string tipo, string region, int cantidad, bool referenciaLimitada, int annio,
 		string artista,
@@ -18,12 +18,19 @@ public:
 		vector<string> canciones) : Material(id, titulo, genero, tipo, region, cantidad, referenciaLimitada, annio),
 		album(album), canciones(canciones), artista(artista) {}
 	// Getters 
-		string getArtista() const { return artista; } 
-		string getAlbum() const { return album; } 
-		vector<string> getCanciones() const { return canciones; } 
-		// Setters 
-		void setArtista(const string& artista) { this->artista = artista; } 
-		void setAlbum(const string& album) { this->album = album; } 
-		void setCanciones(const vector<string>& canciones) { this->canciones = canciones; }
+	string getArtista() const { return artista; }
+	string getAlbum() const { return album; }
+	vector<string> getCanciones() const { return canciones; }
+	// Setters 
+	void setArtista(const string& artista) { this->artista = artista; }
+	void setAlbum(const string& album) { this->album = album; }
+	void setCanciones(const vector<string>& canciones) { this->canciones = canciones; }
+
+	//Override
+	void mostrarInfo() const override
+	{
+		Material::mostrarInfo();
+		cout << "Artista: " << artista << endl;
+		cout << "Album: " << album << endl;
+	}
 };
-	
