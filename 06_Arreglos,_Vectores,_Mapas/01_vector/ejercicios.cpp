ab43fd93ed837a3577d8f1872628abbbff23c9f3
Jose Blancas <92336331+josblax@users.noreply.github.com>
2025-01-26 14:15:21 -0600

    Create ejercicios.cpp

---
+++ 06_Arreglos,_Vectores,_Mapas/01_vector/ejercicios.cpp
@@ -0,0 +1,35 @@
+/* Ejercicio #1
+Realice el programa en C++ que solicite un número e imprima el numero multiplicado por 10 y esto 
+lo seguirá haciendo hasta que el usuario ingrese un cero, guarde los valores en un vector llamado v1. 
+*/
+
+/* Ejercicio #2
+Realice el programa en C++ que encuentre el mayor elemento de un vector de tipo entero llamado v1. 
+Use los siguientes valores:  34,23,94,53,16,27,5,25,42,82
+*/
+
+/* Ejercicio #3
+Realice el programa en C++ que encuentre los tres mayores elementos de un arreglo de 1000 elementos 
+generados de forma aleatoria de tipo entero. Utilice un vector de tipo entero llamado v1.
+*/
+
+/* Ejercicio #4
+Realice el programa en C++ que encuentre el segundo menor elemento de un arreglo de 1000 elementos de 
+tipo double de forma aleatoria. Utilice un vector de tipo float llamado v1.*/
+
+/* Ejercicio #5
+Realice el programa en C++ que encuentre el elemento que más se repita en un vector. El vector debe 
+ser de 1000 elementos de tipo double de forma aleatoria. Utilice Unordered Map.
+*/
+
+/* Ejercicio #6
+Realice el programa en C++ que genere de forma aleatoria 1000 elementos de 
+tipo entero de forma aleatoria. Que separe los elementos pares e impares en dos vectores. 
+Declare dos vectores uno llamado impar y otro par.
+*/
+
+/* Ejercicio #7
+  Realice el programa en C++ que invierta los valores de un arreglo de 10 elementos de tipo entero 
+  de forma aleatoria. Utilice un vector.
+  */
+
